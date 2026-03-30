d38b536abcfb043fc78ff2e78048929adb2451ae
Damian <damian200456@abv.bg>
2026-03-30 22:59:56 +0300

    Added the look of the screen, need to add the logic

--- Snowboard/app/src/main/java/com/example/snowboard/fragments/WeatherScreenFragment.kt
+++ Snowboard/app/src/main/java/com/example/snowboard/fragments/WeatherScreenFragment.kt
@@ -7,15 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.example.snowboard.R
+import com.example.snowboard.databinding.FragmentWeatherScreenBinding
 
 class WeatherScreenFragment : Fragment() {
+    private lateinit var binding: FragmentWeatherScreenBinding
+
+    val API_KEY: String = "84dfac113d43bc77a052c4e6ec5edbb2"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weather_screen, container, false)
+    ): View {
+        binding = FragmentWeatherScreenBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
